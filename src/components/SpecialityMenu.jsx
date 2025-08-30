746ddabe222044a206bfe9859f8b0a58ea5f84d6
MradulGandhi <gandhisir80@gmail.com>
2025-08-30 22:12:16 +0530

    rounded-img radius

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -8,7 +8,7 @@ const SpecialityMenu = () => {
     <div className='flex sm:justify-center gap-4 pt-5 w-full oveerflow-scroll'>
       {specialityData.map((item,index) =>(
         <Link onClick={() => scrollTo(0,0)} className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/Doctors/${item.speciality}`} >
-        <img className='w-16 sm:w-24 mb-2' src= {item.image} />
+        <img className='w-16 sm:w-24 mb-2 rounded-full' src= {item.image} />
         <p>{item.speciality}</p>
 
         </Link>
